245df3f70292f68ba9171bf40a026ebb891f0aa1
bernardo <bekaswashere@gmail.com>
2022-02-05 18:28:34 +0000

    Added Firestore connection

--- update.py
+++ update.py
@@ -1,17 +1,32 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+from google.cloud import firestore
+import config as *
 
-load_dotenv()
+# Global variables
 
-scope = 'user-top-read'
+def spotify_test():
+    results = sp.current_user_top_tracks(limit=50)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    for idx, item in enumerate(results['items']):
+        artist = item['artists'][0]['name']
+        track = item['name']
+        trackid = item['id']
+        print(idx+1, ':', artist, '-', track, '(' + trackid + ')')
 
-results = sp.current_user_top_tracks(limit=50)
 
-for idx, item in enumerate(results['items']):
-    artist = item['artists'][0]['name']
-    track = item['name']
-    trackid = item['id']
-    print(idx+1, ':', artist, '-', track, '(' + trackid + ')')
+if __name__ == '__main__':
+    
+    # Load environment variables stored in .env
+    load_dotenv()
+    print("Credentials loaded.")
+
+    # Connect to Firestore
+    db = firestore.Client(project=firebase_project)
+    print("Connected to Firestore.")
+
+    # Connect to Spotify
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=spotify_scopes))
+    print("Connected to Spotify.")
+
