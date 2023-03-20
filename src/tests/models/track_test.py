8ff4ba8798baf1bca17a5ec45790ef67873d8d04
= <bernardo.jordao@tecnico.ulisboa.pt>
2023-03-20 01:39:37 +0000

    fix ranking import in tests

--- src/tests/models/track_test.py
+++ src/tests/models/track_test.py
@@ -1,7 +1,7 @@
 import spotifystats.models.album as alb
 import spotifystats.models.artist as art
 import spotifystats.models.play as pl
-import spotifystats.models.artist_ranking as rnk
+import spotifystats.models.track_ranking as t_rnk
 import spotifystats.models.track as trk
 
 
