b490d54c4822e06d8e28e1a1325488f726b07ffe
= <bernardo.jordao@tecnico.ulisboa.pt>
2023-03-29 19:17:39 +0100

    ranking and play tests

--- src/tests/models/play_test.py
+++ src/tests/models/play_test.py
@@ -3,20 +3,18 @@ import spotifystats.models.track as trk
 import spotifystats.util as util
 
 
-def test_create_play_from_response(play1):
-    play = pl.Play.from_spotify_response(play1)
+def test_create_play_from_response(play_STEREOTYPE):
+    play = pl.Play.from_spotify_response(play_STEREOTYPE)
 
     assert isinstance(play, pl.Play)
-    assert play.get_track().get_id() == play1["track"]["id"]
-    assert play.get_timestamp() == util.iso_to_datetime(play1["played_at"])
+    assert play.get_track().get_id() == play_STEREOTYPE["track"]["id"]
+    assert play.get_timestamp() == util.iso_to_datetime(play_STEREOTYPE["played_at"])
 
 
-def test_set_track(play1, play1_track, play2_track):
-    play = pl.Play.from_spotify_response(play1)
-    track2 = trk.Track.from_spotify_response(play2_track)
-
-    assert play.get_track().get_id() == play1_track["id"]
+def test_set_track(play_STEREOTYPE, track_LOVE_DIVE):
+    play = pl.Play.from_spotify_response(play_STEREOTYPE)
+    track2 = trk.Track.from_spotify_response(track_LOVE_DIVE)
 
     play.set_track(track2)
 
-    assert play.get_track().get_id() == play2_track["id"]
+    assert play.get_track().get_id() == track_LOVE_DIVE["id"]
--- src/tests/models/track_ranking_test.py
+++ src/tests/models/track_ranking_test.py
@@ -1,22 +1,14 @@
-import spotifystats.models.track as trk
 import spotifystats.models.track_ranking as t_rnk
+from spotifystats.util import iso_to_datetime
 
 
-def test_create_track_ranking_from_response(
-    ranking_tracks_long, top_tracks_long, timestampL
-):
-    ranking = t_rnk.TrackRanking.from_spotify_response(ranking_tracks_long)
-
-    tracks = []
-    for track in top_tracks_long:
-        tracks.append(trk.Track.from_spotify_response(track))
+def test_create_track_ranking_from_response(ranking_track_long):
+    ranking = t_rnk.TrackRanking.from_spotify_response(ranking_track_long)
 
     assert isinstance(ranking, t_rnk.TrackRanking)
-    assert ranking.get_timestamp() == timestampL
+    assert ranking.get_timestamp() == iso_to_datetime(ranking_track_long["timestamp"])
     assert ranking.get_time_range() == "long_term"
     assert len(ranking.get_tracks()) == 50
 
-    tracks = ranking.get_tracks()
-
-    for i in range(50):
-        assert tracks[i].get_id() == top_tracks_long[i]["id"]
+    for i, track in enumerate(ranking.get_tracks()):
+        assert track.get_id() == ranking_track_long["tracks"][i]["id"]
