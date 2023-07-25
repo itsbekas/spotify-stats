6b69a1a336fb9047659e6a6a7dbd63e7b5c098c9
= <bernardo.jordao@tecnico.ulisboa.pt>
2023-07-25 20:29:25 +0100

    limit parameter

--- src/spotifystats/api/app.py
+++ src/spotifystats/api/app.py
@@ -1,19 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 import spotifystats.core.database as db
 
 app = Flask(__name__)
 
 
+# route: /api/v1/top-artists
+# method: GET
+# parameters
+#   - limit: int
+
+
 @app.route("/api/v1/top-artists", methods=["GET"])
-def get_top_artists():
+def get_top_artists(limit: int = 50):
     """Get top artists."""
-    artists = db.get_top_artists()
+    limit = request.args.get("limit", default=50, type=int)
+    artists = db.get_top_artists(limit)
     return jsonify([artist.to_json() for artist in artists])
 
 
 @app.route("/api/v1/top-tracks", methods=["GET"])
-def get_top_tracks():
+def get_top_tracks(limit: int = 50):
     """Get top tracks."""
-    tracks = db.get_top_tracks()
+    limit = request.args.get("limit", default=50, type=int)
+    tracks = db.get_top_tracks(limit)
     return jsonify([track.to_json() for track in tracks])
--- src/spotifystats/core/database/__init__.py
+++ src/spotifystats/core/database/__init__.py
@@ -4,7 +4,7 @@ from spotifystats.core.database.album_db import add_album, get_album
 from spotifystats.core.database.artist_db import add_artist, get_artist, get_top_artists
 from spotifystats.core.database.play_db import add_play, get_latest_timestamp, get_play
 from spotifystats.core.database.ranking_db import add_artist_ranking, add_track_ranking
-from spotifystats.core.database.track_db import add_track, get_track
+from spotifystats.core.database.track_db import add_track, get_top_tracks, get_track
 
 __all__ = [
     "connect",
@@ -21,4 +21,5 @@ __all__ = [
     "add_track_ranking",
     "add_track",
     "get_track",
+    "get_top_tracks",
 ]
