1115c64fbf7f4f3f6a6d36d66987790e8b0ab215
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2023-11-02 20:13:00 +0000

    docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM python:3.11-alpine
 
-# Flask needs SIGINT to exit gracefully (Docker sends SIGTERM by default)
-STOPSIGNAL SIGINT
-
 WORKDIR /app
 
 COPY . /app
@@ -12,7 +9,3 @@ COPY ./.env /app/.env
 RUN pip install -r requirements.txt
 
 RUN pip install .
-
-EXPOSE 5000
-
-CMD ["spotifystats", "run-api"]
--- src/spotifystats/core/service/spotifystats_service.py
+++ src/spotifystats/core/service/spotifystats_service.py
@@ -14,13 +14,28 @@ class SpotifyStatsService:
     def __init__(self):
         self.api = SpotifyAPI()
 
-        mongodb_name = environ.get("SPOTIFYSTATS_MONGODB_DB_NAME")
+        mongodb_user = environ["SPOTIFYSTATS_MONGODB_USER"]
+        if mongodb_user is None:
+            raise ValueError("SPOTIFYSTATS_MONGODB_USER is not set")
+
+        mongodb_password = environ.get("SPOTIFYSTATS_MONGODB_PASSWORD")
+        if mongodb_password is None:
+            raise ValueError("SPOTIFYSTATS_MONGODB_PASSWORD is not set")
+        
+        mongodb_host = environ.get("SPOTIFYSTATS_MONGODB_HOST")
+        if mongodb_host is None:
+            raise ValueError("SPOTIFYSTATS_MONGODB_HOST is not set")
+
+        mongodb_port = environ.get("SPOTIFYSTATS_MONGODB_PORT")
+        if mongodb_port is None:
+            raise ValueError("SPOTIFYSTATS_MONGODB_PORT is not set")
+
+        mongodb_db_name = environ.get("SPOTIFYSTATS_MONGODB_DB_NAME")
         if mongodb_name is None:
             raise ValueError("SPOTIFYSTATS_MONGODB_DB_NAME is not set")
 
-        mongodb_uri = environ.get("SPOTIFYSTATS_MONGODB_URI")
-        if mongodb_uri is None:
-            raise ValueError("SPOTIFYSTATS_MONGODB_URI is not set")
+
+        mongodb_uri = f"mongodb://{mongodb_user}:{mongodb_password}@{mongodb_host}:{mongodb_port}/{mongodb_db_name}"
 
         db.connect(
             name=mongodb_name,
